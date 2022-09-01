2bbcacb3fa6f1d492c67a794a818a4ed28774a41
fragomez <fmgomez60@misena.edu.co>
2022-09-01 08:53:17 -0500

    Creación del index.php para probar

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Frago</title>
+</head>
+
+<body>
+    <section class="contenido">
+        <span class="titulo">Nombre: </span><span class="dato">Franklin Miguel Gómez Bastos</span><br>
+        <span class="titulo">Edad: </span><span class="dato">38 años</span><br>
+        <span class="titulo">Email: </span><span class="dato">fmgomez60@misena.edu.co</span>
+    </section>
+<?php echo "linea php" ?>
+</body>
+
+</html>
